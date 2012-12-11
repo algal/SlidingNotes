1791861ddcaf8752535c0ccaf2567f70b80cf95b
Alexis Gallagher <alexis@alexisgallagher.com>
2012-12-11 00:36:39 +0000

    Initial Commit

---
+++ SlidingNotes/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SlidingNotes
+//
+//  Created by Alexis Gallagher on 2012-12-11.
+//  Copyright (c) 2012 Foxtrot Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SlidingNotes/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SlidingNotes
+//
+//  Created by Alexis Gallagher on 2012-12-11.
+//  Copyright (c) 2012 Foxtrot Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
